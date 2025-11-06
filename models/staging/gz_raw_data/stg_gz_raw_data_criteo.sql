221a61938409b3896fa6619744ed0eab6213a3c3
Jérôme Vander gothen <j.v.gothen@gmail.com>
2025-11-06 11:03:56 +0000

    4 ajouts de campagne pub + campagn name et cast cost en float

---
+++ models/staging/gz_raw_data/stg_gz_raw_data_criteo.sql
@@ -0,0 +1,9 @@
+with src as (
+  select * from {{ source('gz_raw_data','criteo') }}
+)
+
+select
+camPGN_name as campaign_name,
+CAST (ads_cost AS FLOAT64) as ads_cost_clean,
+* except(campGN_name, ads_cost)
+from src
